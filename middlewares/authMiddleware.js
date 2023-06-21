68677a13843bd2ed6cf74116944d1cc07e6a70bd
Utkarsh Rai <utkarshr348@gmail.com>
2023-06-21 15:48:02 +0530

    3rd commit
    added authmiddleware.js to protect user and admin

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,42 @@
+//to protect users
+import JWT from "jsonwebtoken";
+import userModel from "../models/userModel.js";
+//Protected routes token base
+export const requireSignIn = async(req,res,next)=>{
+     //after getting request ,next will be validated then response will be sent
+    try{
+        const decode = JWT.verify(
+            req.headers.authorization,
+            process.env.JWT_SECRET
+        );
+        req.user=decode;
+        next();
+    }catch(error){
+        console.log(error);
+    }
+
+     
+};
+
+//admin access
+export const isAdmin= async (req,res,next)=>{
+    try{
+        const user=await userModel.findById(req.user._id)
+        if(user.role!==1)
+        {
+            return res.status(401).send({
+                success:false,
+                message:"Unauthorized access"
+            })
+        }else{
+            next();
+        }
+    }catch(error){
+        console.log(error);
+        res.status(401).send({
+            success:false,
+            error,
+            message:"error in admin middleware"
+        });
+    }
+};
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,6 +1,6 @@
 import express from "express";
-import {registerController,loginController} from '../controllers/authController.js';
-
+import {registerController,loginController,testController} from '../controllers/authController.js';
+import {isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
 //router object
 const router= express.Router();
 
@@ -11,4 +11,6 @@ router.post('/register',registerController);
 //LOGIN || POST
 router.post('/login',loginController);
 
+//test routes
+router.get('/test',requireSignIn,isAdmin,testController);
 export default router;
