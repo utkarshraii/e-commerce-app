b0ba3e00a5e91fc26dbd174e0ecc09f914cb4047
Utkarsh Rai <utkarshr348@gmail.com>
2023-06-26 03:18:08 +0530

    11th commit
    admin and user panel

--- client/src/components/Layout/Header.js
+++ client/src/components/Layout/Header.js
@@ -62,7 +62,11 @@ const Header = () => {
       {auth?.user?.name}
     </NavLink>
     <ul className="dropdown-menu">
-      <li><NavLink to="/dashboard" className="dropdown-item">Dashboard</NavLink></li>
+      <li><NavLink 
+      to={`/dashboard/${
+        auth?.user?.role===1?'admin':'user'
+        }`} 
+        className="dropdown-item">Dashboard</NavLink></li>
       <li>
       <NavLink onClick={handleLogout}to="/login" className="dropdown-item" >LogOut</NavLink>
       </li>
