42b17c79bb93cf62b6756b6c309eddf185c95378
Utkarsh Rai <utkarshr348@gmail.com>
2023-06-27 04:17:01 +0530

    15th commit
    create product form

--- client/src/pages/Admin/CreateCategory.js
+++ client/src/pages/Admin/CreateCategory.js
@@ -34,8 +34,8 @@ const handleSubmit = async (e) =>{
     const getAllCategory=async()=>{
         try{
             const {data} = await axios.get('/api/v1/category/get-category');
-            if(data.success){
-                setCategories(data.category);
+            if(data?.success){
+                setCategories(data?.category);
             }
         }catch(error){
             console.log(error);
