43d5ff993e4fd9d3c9eaed21be6e56c8dd6c319e
Utkarsh Rai <utkarshr348@gmail.com>
2023-06-27 15:34:19 +0530

    16th commit
    update and delete product

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -36,6 +36,6 @@ router.put(
   router.get("/product-photo/:pid", productPhotoController);
   
   //delete rproduct
-  router.delete("/product/:pid", deleteProductController);
+  router.delete("/delete-product/:pid", deleteProductController);
 
 export default router;
